0941338578629342b82dc13a189fa20af123323d
Yu1stra <aa8523146@gmail.com>
2020-09-13 22:06:27 +0800

    add member.h

---
+++ member.h
@@ -0,0 +1,13 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+#include<QString>
+using namespace std;
+class Member
+{
+private:
+    QString Account;
+    QString Password;
+    QString Name;
+
+};
+#endif // MEMBER_H
